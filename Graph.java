5a1940c8cec23cc07835b1df713c9df366e544bd
Davi Venancio <110872019+DaviVenancio2022@users.noreply.github.com>
2022-08-10 23:35:29 +0000

    Aula pratica 2

--- Graph.java
+++ Graph.java
@@ -2,7 +2,7 @@ public class Graph {
 
 	private int countNodes;
     private int countEdges;
-    private int [][]adjMatrix;
+    private int [][] adjMatrix;
 
     public Graph(int countNodes) {
         this.countNodes = countNodes;
@@ -10,14 +10,14 @@ public class Graph {
     }
 
     public int getCountNodes() {
-        return countNodes;
+        return this.countNodes;
     }
 
     public int getCountEdges() {
-        return countEdges;
+        return this.countEdges;
     }
 
-    public int[][] getAdjMatrix() {
+    public int [][] getAdjMatrix() {
         return this.adjMatrix;
     }
 
@@ -40,20 +40,55 @@ public class Graph {
 			+ sink + " " + weight);
 			return;
 	    }
-	    this.adjMatrix[source][sink]= weight;
+	    this.adjMatrix[source][sink] = weight;
 	    this.countEdges++;
     }
   
   	public int degree(int node) {
-  	    if(node < 0 || node> this.countNodes -1) {
+  	    if(node < 0 || node > this.countNodes -1) {
       		System.out.println("Invalid node: "  + node);
 		}
-    	int degree= 0;
-	    for(int j= 0; j< this.adjMatrix[node].length; ++j) {
+    	int degree = 0;
+	    for(int j = 0; j < this.adjMatrix[node].length; ++j) {
 		    if(this.adjMatrix[node][j] != 0) {
 		        ++degree;
 		    }
 		}
 		return degree;
     }
+
+	public int highestDegree() {
+		int highest = 0; // maior = 0;
+		for(int i = 0; i < this.adjMatrix.length; ++i) { // ler linhas da matriz = 4
+			int aux = this.degree(i); // auxiliar para evitar chamar a funcao toda hora
+			if(highest < aux ) { // comparar todas os nos
+				highest = aux;
+			}
+		}
+		return highest;
+	}
+
+	public int lowestDegree() {
+		int lowest = 0; // menor = 0;
+		for(int i = 0; i < this.adjMatrix.length; ++i) { // ler linhas da matriz = 4
+			int aux = this.degree(i); // auxiliar para evitar chamar a funcao toda hora
+			if(lowest > aux ) { // comparar todas os nos
+				lowest = aux;
+			}
+		}
+		return lowest;
+	}
+
+	public Graph complement() {
+		Graph g2 = new Graph(this.countNodes);
+		for(int i = 0; i < this.adjMatrix.length; ++i) {
+			for (int j = 0; j < this.adjMatrix[i].length; ++j) {
+				if(this.adjMatrix[i][j] == 0 && i != j) {
+					//g2.adjMatriz[i][j]= 1;
+					g2.addEdge(i, j, 1);
+				}
+			}
+		}
+		return g2;
+	}
 }
--- Main.java
+++ Main.java
@@ -7,10 +7,22 @@ class Main {
    		g1.addEdge(0, 3, 4);
    		g1.addEdge(3, 0, 4);
     	g1.addEdge(3, 4, 4);//warning
+
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Matriz: ");
 	    System.out.println(g1);
+		System.out.println("---------------------------------------------");
+		System.out.println("Tamanho nodes de cada linha
+						   da matriz: ");
 		System.out.println(g1.degree(0));
 		System.out.println(g1.degree(1));
 		System.out.println(g1.degree(2));
 		System.out.println(g1.degree(3));
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Node de maior grau: " + g1.highestDegree());
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Node de menor grau: " + g1.lowestDegree());
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Matriz complementar: \n" + g1.complement());
     }
 }
