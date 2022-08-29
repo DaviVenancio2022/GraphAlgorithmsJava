b43764a6e99e67f10f5b15c303a6cdedcb05e6a7
Davi Venancio <110872019+DaviVenancio2022@users.noreply.github.com>
2022-08-29 03:47:58 +0000

    att

--- Graph.java
+++ Graph.java
@@ -1,23 +1,29 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.IOException;
+
 public class Graph {
 
     private int countNodes;
     private int countEdges;
-    private int [][]adjMatrix;
+    private int [][] adjMatrix;
 
-    public Graph(int countNodes) {
+    public Graph(int countNodes) { // metodo construtor recebe n de vertices/nos
         this.countNodes = countNodes;
-        this.adjMatrix = new int[countNodes][countNodes];
+        this.adjMatrix = new int[countNodes][countNodes]; // cria matriz de adj quadrada
     }
 
     public int getCountNodes() {
-        return countNodes;
+        return this.countNodes;
     }
 
     public int getCountEdges() {
-        return countEdges;
+        return this.countEdges;
     }
 
-    public int[][] getAdjMatrix() {
+    public int [][] getAdjMatrix() {
         return this.adjMatrix;
     }
 
@@ -31,29 +37,75 @@ public class Graph {
         }
         return str;
     }
-	
+
     public void addEdge(int source, int sink, int weight) {
-	if(source < 0 || source > this.countNodes -1
-	   || sink < 0 || sink > this.countNodes -1
-	   || weight <= 0) {
-	    System.err.println("Invalid adge: " + " " + source + " " 
-	    + sink + " " + weight);
+        if(source < 0 || source > this.countNodes -1 || sink < 0 || sink > this.countNodes -1 || weight <= 0) {
+	    System.err.println("Invalid adge: " + " " + source + " " + sink + " " + weight);
 	    return;
 	}
-	this.adjMatrix[source][sink]= weight;
-        this.countEdges++;
+	this.adjMatrix[source][sink] = weight;
+	this.countEdges++;
+    }
+
+	public void addEdgeUnoriented(int u, int v, int w) { //adc arestas= origem, destino, peso
+	    if(u < 0 || u > this.countNodes -1
+		   || v < 0 || v > this.countNodes -1
+		   || w <= 0) {
+			System.out.println("\n---------------------------------------------");
+	        System.err.println("Invalid adge: " + " " + u + " " 
+			+ v + " " + w);
+			return;
+	    }
+	    this.adjMatrix[u][v] = w;
+		this.adjMatrix[v][u] = w;
+	    this.countEdges += 1;
     }
   
     public int degree(int node) {
- 	if(node < 0 || node> this.countNodes -1) {
+        if(node < 0 || node > this.countNodes -1) {
       	    System.out.println("Invalid node: "  + node);
 	}
-    	int degree= 0;
-	for(int j= 0; j< this.adjMatrix[node].length; ++j) {
-	   if(this.adjMatrix[node][j] != 0) {
-	      ++degree;
-	   }
+    	int degree = 0;
+	for(int j = 0; j < this.adjMatrix[node].length; ++j) {
+	    if(this.adjMatrix[node][j] != 0) {
+	        ++degree;
+	    }
 	}
 	return degree;
+    }
+
+    public int highestDegree() {
+        int highest = 0; // maior = 0;
+	for(int i = 0; i < this.adjMatrix.length; ++i) { // ler linhas da matriz = 4
+	    int aux = this.degree(i); // auxiliar para evitar chamar a funcao toda hora
+	    if(highest < aux ) { // comparar todas os nos
+	        highest = aux;
+	    }
+	}
+	return highest;
+    }
+
+    public int lowestDegree() {
+	int lowest = 0; // menor = 0;
+	for(int i = 0; i < this.adjMatrix.length; ++i) { // ler linhas da matriz = 4
+	    int aux = this.degree(i); // auxiliar para evitar chamar a funcao toda hora
+	    if(lowest > aux ) { // comparar todas os nos
+	        lowest = aux;
+	    }
+	}
+        return lowest;
+    }
+
+    public Graph complement() {
+	Graph g2 = new Graph(this.countNodes);
+	for(int i = 0; i < this.adjMatrix.length; ++i) {
+	    for (int j = 0; j < this.adjMatrix[i].length; ++j) {
+		if(this.adjMatrix[i][j] == 0 && i != j) {
+	  	  //g2.adjMatriz[i][j]= 1;
+		    g2.addEdge(i, j, 1);
+		}
+	    }
+	}
+	return g2;
     }
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,92 @@
+import java.io.IOException;
+
 class Main {
-	
-    public static void main(String[] args) {
-	Graph g1 = new Graph(4);
-	g1.addEdge(0, 1, 3);
-    	g1.addEdge(1, 0, 3);
-   	g1.addEdge(0, 3, 4);
-   	g1.addEdge(3, 0, 4);
-    	g1.addEdge(3, 4, 4);//warning
-	System.out.println(g1);
-	System.out.println(g1.degree(0));
-	System.out.println(g1.degree(1));
-	System.out.println(g1.degree(2));
-	System.out.println(g1.degree(3));
+
+  	public static void main(String[] args) throws IOException {
+		/*
+		Graph g1 = new Graph(4); // criando um grafo tamanho 4x4
+		g1.addEdge(0, 1, 3); // adc arestas, recebendo origem/x, destino/y e peso/valor
+    	g1.addEdge(1, 0, 3); // (1, 0)= 3
+   		g1.addEdge(0, 3, 4); // (0, 3)= 4
+   		g1.addEdge(3, 0, 4); // (3, 0)= 4
+    	g1.addEdge(3, 4, 4); // (3, 4)= 4  warning!! y nao pode receber psÇ maior q 3
+		
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Matriz: ");
+	    System.out.println(g1);
+		System.out.println("Matriz g1: ");
+	    System.out.println(g1); // imprimindo matriz
+		System.out.println("---------------------------------------------");
+		System.out.println("Tamanho nodes de cada linha
+						   da matriz: ");
+		System.out.println("Grau de cada linha/nó da matriz: "); // linhas sao nos?
+		System.out.println(g1.degree(0));
+		System.out.println(g1.degree(1));
+		System.out.println(g1.degree(2));
+		System.out.println(g1.degree(3));
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Node de maior grau: " + g1.highestDegree());
+		System.out.println("Nó com maior grau: " + g1.highestDegree());
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Node de menor grau: " + g1.lowestDegree());
+		System.out.println("Nó de menor grau: " + g1.lowestDegree());
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Matriz complementar: \n" + g1.complement());
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Densidade matriz g1: \n" + g1.density());
+		
+		Graph g2 = new Graph(3);
+	    g2.addEdge(0, 1, 1);
+	    g2.addEdge(1, 0, 1);
+		System.out.println("\n---------------------------------------------");
+	    System.out.println("g2 e subgrafo de g1? " + g1.subGraph(g2)); // true
+		System.out.println("\nDensidade de g2: \n" + g2.density());
+		
+	    Graph g3 = new Graph(4);
+	    g3.addEdge(1, 3, 1);
+	    g3.addEdge(3, 1, 1);
+		System.out.println("\n---------------------------------------------");
+	    System.out.println("g3 e subgrafo de g1? " + g1.subGraph(g3)); // false
+		System.out.println("\nDensidade de g3: \n" + g3.density());
     }
+	*/
+		Graph g1 = new Graph(9); // nos= 9
+		g1.addEdgeUnoriented(7, 5, 1);
+		g1.addEdgeUnoriented(7, 1, 1);
+		g1.addEdgeUnoriented(7, 2, 1);
+		g1.addEdgeUnoriented(1, 0, 1);
+		g1.addEdgeUnoriented(1, 4, 1);
+		g1.addEdgeUnoriented(2, 3, 1);
+		g1.addEdgeUnoriented(5, 6, 1);
+		g1.addEdgeUnoriented(6, 8, 1);
+
+		System.out.println("\n---------------------------------------------");
+		System.out.println("Grafo g1:\n");
+		System.out.println(g1);
+		System.out.println("\nLista de adjascencia/busca em largura: ");
+		System.out.println(g1.bfs(7)); // Inicializaçao da busca s=7
+		System.out.println("\n---------------------------------------------");
+		System.out.println("\nGrafo g1 e conexo? ");
+		System.out.println(g1.connected());
+
+		System.out.println("\n---------------------------------------------");
+		System.out.println("\nGrafo g2 txt:\n");
+		Graph g2 = new Graph("Graph.txt");
+		System.out.println(g2);
+
+		System.out.println("\n---------------------------------------------");
+		System.out.println("\nGrafo g3:\n");
+		Graph g3 = new Graph(7);
+		g3.addEdgeUnoriented(6, 3, 1);
+		g3.addEdgeUnoriented(6, 4, 1);
+		g3.addEdgeUnoriented(6, 5, 1);
+		g3.addEdgeUnoriented(3, 2, 1);
+		g3.addEdgeUnoriented(4, 0, 1);
+		g3.addEdgeUnoriented(0, 1, 1);
+
+		System.out.println(g3);
+		System.out.println("\n---------------------------------------------");
+		System.out.println("\nLista de adjascencia/busca em profundidade: ");
+		System.out.println(g3.dfs(6)); // Inicializaçao da busca s=6
+	}
 }
